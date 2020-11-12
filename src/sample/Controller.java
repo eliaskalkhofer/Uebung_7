2c155d8913a89281705b0b63d9d72b2bc2fdb6d5
Elias Kalkhofer <elias.kalkhofer@htl-wels.at>
2020-11-12 10:16:59 +0100

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,10 @@
+package sample;
+
+/**
+ * @class: Controller
+ * @author: Elias Kalkhofer
+ * @date: 12.11.2020
+ */
+
+public class Controller {
+}
---
+++ src/sample/Main.java
@@ -0,0 +1,29 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * @class: Main
+ * @author: Elias Kalkhofer
+ * @date: 12.11.2020
+ */
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
